b9669064f2ade7407a43f16422410f632718fb6f
Gabrielly Barros Fonseca <gabrielly.barros@hotmail.com>
2022-03-06 20:20:02 -0300

    Responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text>Gabrielly Fonseca</Text>
-        <Text color='gray.300' fontSize='small'>
-          gabrielly.barros@hotmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Gabrielly Fonseca</Text>
+          <Text color='gray.300' fontSize='small'>
+            gabrielly.barros@hotmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size='md'
