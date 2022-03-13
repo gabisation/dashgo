b3411bcf3142e1b4c0933253708cb3e731fa713b
Gabrielly Barros Fonseca <gabrielly.barros@hotmail.com>
2022-03-13 19:01:44 -0300

    Axios config

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,17 +16,17 @@ import {
   useBreakpointValue,
 } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import Link from 'next/link';
+import { useQuery } from 'react-query';
 
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
-import Link from 'next/link';
-import { useQuery } from 'react-query';
+import { api } from '../../services/api';
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+    const { data } = await api('users')
     
     const users = data.users.map(user => {
       return {
